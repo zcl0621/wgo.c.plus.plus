9d45f3b9dc6a7ab29fb742d86bce3ffbb22b7be1
zhangchaolong <zcl0621@hotmail.com>
2025-08-02 00:01:32 +0800

    initial commit

---
+++ color.h
@@ -0,0 +1,6 @@
+//
+// Created by 张朝龙 on 25-8-1.
+//
+#pragma once
+
+enum Color { BLACK, WHITE, EMPTY };
---
+++ store.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 张朝龙 on 25-8-1.
+//
+#include "color.h"
+
+class Store {
+public:
+    int x;
+    int y;
+    Color c;
+
+    Store(const int x, const int y, const Color c) : x(x), y(y), c(c) {}
+};
